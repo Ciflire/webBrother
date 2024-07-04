a5981ef2211c935d9ba25e71d36d72ffca1a0951
VESSE Léo <leovesse@gmail.com>
2024-07-04 17:06:19 +0200

    adding dev setup
    .direnv added to .gitignore
    flake added
    direnv added

---
+++ flake.nix
@@ -0,0 +1,25 @@
+# flake.nix
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          cargo
+          rustc
+          rust-analyzer
+          rustfmt
+        ];
+        shellHook = "";
+      };
+
+    };
+}
